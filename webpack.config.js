36e93e651d399027d41cfcf58cb16e1f75594a43
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2022-03-11 22:15:07 +0900

    Bump up

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist"
-  },
-
   mode: "development",
-
-  // Enable sourcemaps for debugging webpack"s output.
   devtool: "source-map",
-
   resolve: {
-    // Add ".ts" and ".tsx" as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
-
   module: {
     rules: [
-      // All files with a ".ts" or ".tsx" extension will be handled by "awesome-typescript-loader".
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-      // All output ".js" files will have any sourcemaps re-processed by "source-map-loader".
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-    ]
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
   },
 
+  devServer: {
+    hot: true,
+    open: true,
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
 };
