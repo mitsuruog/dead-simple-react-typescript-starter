2a845d1ee3c5fc5a8e5e89b6c37ee7e0d4687424
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2022-05-04 14:07:23 +0900

    Bump up

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -7,6 +7,11 @@ export interface HelloProps {
 
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return (
+      <h1>
+        Hello from {this.props.compiler} and {this.props.framework}!
+        <button onClick={() => console.log("Hello")}>click</button>
+      </h1>
+    );
   }
 }
